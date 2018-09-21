ee0e52b4fa835fed6c8e03ca8ca1b406a623b8a1
Voxoff <guybennettjones@hotmail.co.uk>
2018-09-21 20:16:25 +0100

    I really need to learn functions

--- pset3/music/helpers.c
+++ pset3/music/helpers.c
@@ -4,31 +4,50 @@
 #include <stdio.h>
 #include <string.h>
 #include "helpers.h"
+#include <math.h>
 
 // Converts a fraction formatted as X/Y to eighths
 int duration(string fraction)
 {
-    if(fraction)
-    {
-      return 2;
-    }
+  int math = fraction[2] / 8; 
+  return fraction[0] / math;
   return -1;
 }
 
 // Calculates frequency (in Hz) of a note
 int frequency(string note)
 {
-    if(note)
-    {
-      return 440;
-    }
+  if((int) note[1] == 35 || (int) note[1] == 98)
+  {
+    int oct = atoi(&note[2]);
+    int diff_from_mid = oct - 4;
+    int freq = (int) note[0] - 65;
+    int scale = (freq == 0 || freq == 1) ? freq : freq - 8;
+    int n = (diff_from_mid * 8) - scale;
+    n = strcmp(&note[1], "#") == 0 ? n + 1 : n -1;
+    float ffs = n / 12.00;
+    float frequency = pow(2.00, ffs) * 440;
+    printf("%i\n", (int) frequency);
+    return (int) frequency;
+  }
+  else
+  {
+    int oct = atoi(&note[1]);
+    int diff_from_mid = oct - 4;
+    int freq = (int) note[0] - 65;
+    int scale = (freq == 0 || freq == 1) ? freq : freq - 8;
+    int n = (diff_from_mid * 8) - scale;    
+    float ffs = n / 12.00;
+    float frequency = pow(2.00, ffs) * 440;
+    printf("%i\n", (int) frequency);
+    return (int) frequency;
+  }
   return -1;
 }
 
 // Determines whether a string represents a rest
 bool is_rest(string s)
 {
-    printf("%s", s);
     if(strcmp(s,"") == 0)
     {
       return true;
