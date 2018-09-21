2d19569d0fe5b184c9835a280f783d62687caf7f
Voxoff <guybennettjones@hotmail.co.uk>
2018-09-21 20:28:02 +0100

    imademusic

--- pset3/music/helpers.c
+++ pset3/music/helpers.c
@@ -19,15 +19,15 @@ int frequency(string note)
 {
   if((int) note[1] == 35 || (int) note[1] == 98)
   {
+    // G4 => 415
     int oct = atoi(&note[2]);
     int diff_from_mid = oct - 4;
     int freq = (int) note[0] - 65;
     int scale = (freq == 0 || freq == 1) ? freq : freq - 8;
-    int n = (diff_from_mid * 8) - scale;
+    int n = (diff_from_mid * 8) + scale;
     n = strcmp(&note[1], "#") == 0 ? n + 1 : n -1;
     float ffs = n / 12.00;
-    float frequency = pow(2.00, ffs) * 440;
-    printf("%i\n", (int) frequency);
+    float frequency = (pow(2.00, ffs) * 440);
     return (int) frequency;
   }
   else
@@ -36,10 +36,9 @@ int frequency(string note)
     int diff_from_mid = oct - 4;
     int freq = (int) note[0] - 65;
     int scale = (freq == 0 || freq == 1) ? freq : freq - 8;
-    int n = (diff_from_mid * 8) - scale;    
+    int n = (diff_from_mid * 8) + scale;    
     float ffs = n / 12.00;
     float frequency = pow(2.00, ffs) * 440;
-    printf("%i\n", (int) frequency);
     return (int) frequency;
   }
   return -1;
